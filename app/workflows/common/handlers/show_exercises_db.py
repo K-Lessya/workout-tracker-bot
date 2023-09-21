b54ec2fa92eef7b1a5a5e31c07f129326b3e795a
Andrei Ryzhykau <andrei.ryzhykau@outstaff.exness.com>
2023-09-21 10:17:45 +0200

    fix registration upload photo handler

--- app/workflows/common/handlers/show_exercises_db.py
+++ app/workflows/common/handlers/show_exercises_db.py
@@ -3,14 +3,13 @@ from aiogram import Router
 from aiogram import F
 from app.bot import bot
 from aiogram.fsm.context import FSMContext
-from aiogram.types import CallbackQuery, FSInputFile, URLInputFile, Message
+from aiogram.types import CallbackQuery, URLInputFile, Message
 
 from app.keyboards.yes_no import YesNoKeyboard
 from app.utilities.default_callbacks.default_callbacks import ChooseCallback, MoveToCallback
-from app.utilities.default_keyboards.yes_no import create_yes_no_keyboard
-from app.workflows.common.utils.callback_properties.movetos import UpstreamMenuMoveTo, CommonGoBackMoveTo
-from app.workflows.common.utils.callback_properties.targets import ExerciseDbTargets
-from app.workflows.common.utils.keyboards.exercise_db_choose import create_exercise_db_choose_keyboard
+from app.workflows.common.utils.callback_properties.movetos import UpstreamMenuMoveTo
+from app.workflows.common.utils.keyboards.exercise_db_choose import MuscleGroupsKeyboard, ExercisesListKeyboard,\
+    ExerciseKeyboard
 from ..utils.callback_properties.targets import ExerciseDbTargets
 from app.entities.exercise.crud import *
 from app.s3.downloader import create_presigned_url
@@ -20,27 +19,45 @@ from app.entities.single_file.crud import get_trainer
 from app.workflows.trainer.utils.states import TrainerStates
 from ...trainer.utils.callback_properties.targets import CreateExerciseTargets
 from app.translations.base_translations import translations
+from app.workflows.common.utils.keyboards.exercise_db_choose import BodyPartsKeyboard
 
 show_exercises_db_router = Router()
 
 @show_exercises_db_router.callback_query(MoveToCallback.filter(F.move_to == UpstreamMenuMoveTo.show_exercise_db))
 @callback_error_handler
 async def list_body_parts(callback: CallbackQuery, callback_data: ChooseCallback, state: FSMContext):
-    trainer =get_trainer(callback.from_user.id)
+    trainer = get_trainer(callback.from_user.id)
     lang = trainer.lang
     await state.update_data({'lang': lang})
-
+    state_data = await state.get_data()
     body_parts = get_all_body_parts(trainer)
+    start_index = state_data.get('body_part_index', 0)
+    keyboard = BodyPartsKeyboard(body_parts, start_index, lang)
+    await state.update_data({'current_keyboard': keyboard})
     await state.set_state(TrainerStates.exercises_db.process_buttons.choose_body_part)
     await callback.message.edit_text(translations[lang].trainer_exercise_db_menu_choose_body_part.value,
-                                     reply_markup=create_exercise_db_choose_keyboard(
-                                         options=body_parts,
-                                         source=callback,
-                                         target=ExerciseDbTargets.show_body_part,
-                                         go_back_filter=MoveToCallback(move_to=CommonGoBackMoveTo.to_trainer_main_menu),
-                                         lang=lang
-                                     ))
+                                     reply_markup=keyboard.ui.as_markup())
     await callback.answer()
+
+
+
+@show_exercises_db_router.callback_query(MoveToCallback.filter((F.move_to == "next") | (F.move_to == "prev")))
+async def mover(callback: CallbackQuery, callback_data: MoveToCallback, state: FSMContext):
+    state_data = await state.get_data()
+    keyboard = state_data.get('current_keyboard')
+    if callback_data.move_to == 'next':
+        keyboard.list_forward()
+        await callback.message.edit_reply_markup(reply_markup=keyboard.ui.as_markup())
+    else:
+        keyboard.list_backward()
+        await callback.message.edit_reply_markup(reply_markup=keyboard.ui.as_markup())
+    if isinstance(state_data.get('current_keyboard'), BodyPartsKeyboard):
+        await state.update_data({'body_part_index': keyboard.start_index})
+    elif isinstance(state_data.get('current_keyboard'), MuscleGroupsKeyboard):
+        await state.update_data({'muscle_group_index': keyboard.start_index})
+    elif isinstance(state_data.get('current_keyboard'), ExercisesListKeyboard):
+        await state.update_data({'exercise_index': keyboard.start_index})
+
 
 @show_exercises_db_router.callback_query(ChooseCallback.filter(F.target == ExerciseDbTargets.show_body_part))
 @callback_error_handler
@@ -53,14 +70,11 @@ async def show_muscle_groups(callback: CallbackQuery, callback_data: ChooseCallb
     await state.set_state(TrainerStates.exercises_db.process_buttons.choose_muscle_group)
     await state.update_data({'body_part': body_part})
     muscle_groups = get_muscle_groups_by_body_part(body_part=body_part.id)
+    start_index = state_data.get('muscle_group_index', 0)
+    keyboard = MuscleGroupsKeyboard(muscle_groups, start_index, lang)
+    await state.update_data({'current_keyboard': keyboard})
     await callback.message.edit_text(translations[lang].trainer_exercise_db_menu_choose_muscle_group.value,
-                                     reply_markup=create_exercise_db_choose_keyboard(
-                                         options=muscle_groups,
-                                         source=callback,
-                                         target=ExerciseDbTargets.show_muscle_group,
-                                         go_back_filter=MoveToCallback(move_to=UpstreamMenuMoveTo.show_exercise_db),
-                                         lang=lang
-                                     ))
+                                     reply_markup=keyboard.ui.as_markup())
     await callback.answer()
 
 @show_exercises_db_router.callback_query(ChooseCallback.filter(F.target == ExerciseDbTargets.show_muscle_group))
@@ -75,28 +89,17 @@ async def show_muscle_groups(callback: CallbackQuery, callback_data: ChooseCallb
     exercises = get_exercises_by_muscle_group(muscle_group=muscle_group.id)
     await state.update_data({'muscle_group': muscle_group})
     print(exercises)
+    start_index = state_data.get('exercise_index', 0)
+    keyboard = ExercisesListKeyboard(exercises, start_index, go_back_ref, lang)
+    await state.update_data({'current_keyboard': keyboard})
     if 'has_media' in state_data.keys():
         await bot.send_message(chat_id=callback.from_user.id,
                                text=translations[lang].trainer_exercise_db_menu_choose_exercise.value,
-                               reply_markup=create_exercise_db_choose_keyboard(
-                                   options=exercises,
-                                   source=callback,
-                                   target=ExerciseDbTargets.show_exercise,
-                                   go_back_filter=ChooseCallback(target=ExerciseDbTargets.show_body_part,
-                                                                 option=go_back_ref),
-                                   lang=lang
-                               ))
+                               reply_markup=keyboard.ui.as_markup())
         await callback.message.delete()
     else:
         await callback.message.edit_text(translations[lang].trainer_exercise_db_menu_choose_exercise.value,
-                                         reply_markup=create_exercise_db_choose_keyboard(
-                                             options=exercises,
-                                             source=callback,
-                                             target=ExerciseDbTargets.show_exercise,
-                                             go_back_filter=ChooseCallback(target=ExerciseDbTargets.show_body_part,
-                                                                           option=go_back_ref),
-                                             lang=lang
-                                         ))
+                                         reply_markup=keyboard.ui.as_markup())
     await callback.answer()
 
 
@@ -112,19 +115,13 @@ async def show_exercise(callback: CallbackQuery, callback_data: ChooseCallback, 
     media_link = create_presigned_url(PHOTO_BUCKET, exercise.media_link)
     print(media_link)
     go_back_ref = str(state_data['muscle_group'].id)
-    keyboard = create_exercise_db_choose_keyboard(
-                                 options=None,
-                                 source=callback,
-                                 target="Exercise",
-                                 lang=lang,
-                                 go_back_filter=ChooseCallback(target=ExerciseDbTargets.show_muscle_group,
-                                                               option=go_back_ref))
+    keyboard = ExerciseKeyboard(exercise, go_back_ref, lang)
 
     if exercise.media_type == 'photo':
         await state.update_data({'has_media': True})
         await bot.send_photo(chat_id=callback.from_user.id, photo=media_link,
                              caption=f"{exercise.name}",
-                             reply_markup=keyboard)
+                             reply_markup=keyboard.as_markup())
     elif exercise.media_type == 'video':
         await state.update_data({'has_media': True})
 
@@ -132,7 +129,7 @@ async def show_exercise(callback: CallbackQuery, callback_data: ChooseCallback, 
         print(file)
         await bot.send_video(chat_id=callback.from_user.id, video=file,
                              caption=f'{exercise.name}',
-                             reply_markup=keyboard)
+                             reply_markup=keyboard.as_markup())
     await callback.message.delete()
 
 
--- app/workflows/common/utils/keyboards/exercise_db_choose.py
+++ app/workflows/common/utils/keyboards/exercise_db_choose.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardButton, Message, CallbackQuery
 from app.utilities.default_keyboards.choose import create_choose_keyboard
 from app.utilities.default_callbacks.default_callbacks import MoveToCallback, ChooseCallback
-from app.workflows.common.utils.callback_properties.movetos import ExerciseDbMoveTo
+from app.workflows.common.utils.callback_properties.movetos import ExerciseDbMoveTo, UpstreamMenuMoveTo
 from app.workflows.trainer.utils.callback_properties.targets import CreateExerciseTargets
 from app.workflows.common.utils.callback_properties.targets import ExerciseDbTargets
 from typing import Optional
@@ -13,6 +13,7 @@ from app.entities.exercise.crud import *
 from app.workflows.trainer.utils.callback_properties.targets import TrainerMyClientsTargets
 from app.workflows.client.utils.callback_properties.targets import ClientAddCustomTrainingTargets
 from app.translations.base_translations import translations
+from app.config import CHOOSE_BUTTON_MAX_COUNT_PER_PAGE
 
 
 def create_add_exercise_button(lang):
@@ -108,6 +109,124 @@ class ExerciseCommonListKeyboard(InlineKeyboardBuilder):
                         callback_data=ChooseCallback(
                             target=target,
                             option=str(item.id)).pack()))
+
+
+
+
+
+class CategoryPaginationKeyboard:
+
+    start_index: int
+    count: int
+    options: list
+    target: str
+    lang: str
+    option_attr: any
+    ui: InlineKeyboardBuilder
+
+    def __init__(self, options, target, option_attr, count, start_index, lang, go_back_callback_data,
+                 additional_btns=list):
+        super().__init__()
+        self.start_index = start_index
+        self.count = count
+        self.options = options
+        self.target = target
+        self.option_attr = option_attr
+        self.lang = lang
+        self.go_back_callback_data = go_back_callback_data
+        self.draw_keyboard(additional_btns=additional_btns)
+
+    def draw_keyboard(self, additional_btns):
+        self.ui = InlineKeyboardBuilder()
+        options_list = []
+        for option in self.options:
+            options_list.append(option)
+        if len(options_list) < self.count:
+            for option in options_list:
+                self.ui.row(InlineKeyboardButton(
+                    text=option.name,
+                    callback_data=ChooseCallback(target=self.target, option=str(getattr(option, self.option_attr))).pack()
+                ))
+        else:
+            for option in options_list[self.start_index:self.start_index+self.count:1]:
+                self.ui.row(InlineKeyboardButton(
+                    text=option.name,
+                    callback_data=ChooseCallback(target=self.target, option=str(getattr(option, self.option_attr))).pack()
+                ))
+            move_buttons = []
+            if self.start_index > 0:
+                move_buttons.append(
+                    InlineKeyboardButton(
+                        text="<<",
+                        callback_data=MoveToCallback(move_to="prev").pack()
+                    )
+                )
+            if self.start_index+self.count-1 < len(self.options)-1:
+                move_buttons.append(
+                    InlineKeyboardButton(
+                        text=">>",
+                        callback_data=MoveToCallback(move_to="next").pack()
+                    )
+                )
+            self.ui.row(*move_buttons)
+            if additional_btns:
+                self.ui.row(*additional_btns)
+            self.ui.row(InlineKeyboardButton(
+                text=translations[self.lang].go_back_btn.value,
+                callback_data=self.go_back_callback_data.pack()
+            ))
+
+    def list_forward(self):
+        self.start_index += self.count
+        self.draw_keyboard()
+
+    def list_backward(self):
+        self.start_index -= self.count
+        self.draw_keyboard()
+
+
+class BodyPartsKeyboard(CategoryPaginationKeyboard):
+
+    def __init__(self, body_parts, start_index, lang, create_exercise_btn: bool):
+        go_back_callback = MoveToCallback(move_to=CommonGoBackMoveTo.to_trainer_main_menu)
+        if create_exercise_btn:
+            additional_btns = [
+                InlineKeyboardButton(
+                    text=translations[lang].trainer_add_exercise_btn_add_exercise.value,
+                    callback_data=
+                )
+            ]
+        super().__init__(body_parts, ExerciseDbTargets.show_body_part, 'id', 1, start_index, lang, go_back_callback)
+
+
+class MuscleGroupsKeyboard(CategoryPaginationKeyboard):
+
+    def __init__(self, muscle_groups, start_index, lang):
+        go_back_callback = MoveToCallback(move_to=UpstreamMenuMoveTo.show_exercise_db)
+        super().__init__(muscle_groups, ExerciseDbTargets.show_muscle_group, 'id', 1, start_index, lang, go_back_callback)
+
+class ExercisesListKeyboard(CategoryPaginationKeyboard):
+
+    def __init__(self, exercises, start_index, source_option, lang):
+        go_back_callback = ChooseCallback(target=ExerciseDbTargets.show_body_part, option=source_option)
+        super().__init__(exercises, ExerciseDbTargets.show_exercise, 'id', 1, start_index, lang,
+                         go_back_callback)
+
+class ExerciseKeyboard(InlineKeyboardBuilder):
+    def __init__(self, exercise, source_option, lang):
+        super().__init__()
+        self.row(
+            InlineKeyboardButton(text=translations[lang].go_back_btn.value,
+                                 callback_data=ChooseCallback(target=ExerciseDbTargets.show_muscle_group,
+                                                              option=source_option).pack())
+        )
+
+
+
+
+
+
+
 
 
 
